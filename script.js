ecf69cc08eb5995f837d5b108a6dbae626723c3c
Dev-Sarmad <sarmadh230@gmail.com>
2023-01-22 21:12:33 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById('menu-btn');
+let menu= document.getElementById('menu');
+btn.addEventListener('click' , navToggle)
+
+function navToggle(){
+    btn.classList.toggle('open')
+    menu.classList.toggle('flex')
+    menu.classList.toggle('hidden')
+}
